ff34d5ce998b1a2e32f0d8cd93756c51be8a9b81
achitta <46438987+achitta@users.noreply.github.com>
2022-03-28 20:14:00 -0400

    ES Tree init functionality

--- driver.py
+++ driver.py
@@ -1,7 +1,14 @@
 from es_tree import EsTree
 
+
+#        0
+#      /  \
+#     1    2
+#    / \     \
+#   3   4  -  5
+
 def main():
-    e = EsTree(3, [(0,1), (1,2), (0,2)], 1)
+    e = EsTree(6, [(0,1), (0,2), (1,3), (1,4), (2,5), (4,5)], 0)
 
 if __name__ == "__main__":
     main()
--- es_tree.py
+++ es_tree.py
@@ -1,4 +1,4 @@
-
+from collections import deque
 
 class Node:
     def __init__(self, id):
@@ -12,20 +12,53 @@ class Node:
         self.unchecked = set()
     
     def __str__(self):
-        return f"Node(id: {self.id}, level: {self.level}, parent: {self.parent})"
+        return f"Node(\n\tid: {self.id},\n\tlevel: {self.level},\n\tparent: {self.parent},\
+                \n\tnonNeighbors: {self.nonNeighbors},\n\tunchecked: {self.unchecked})\n"
     
     def __repr__(self) -> str:
         return self.__str__()
     
 class EsTree:
     def __init__(self, numVertices, edges, srcNode):
-        self.adjacencyMatrix = {}
-        self.idToNode = {}
+        """Initialize ES Tree."""
+        self.adjacencyMatrix = {} # Map from node id to set of neighbor ids
+        self.idToNode = {} # Map from node id to node object
+        self.srcNode = srcNode # Source node id
         
-        # Init data structures
         for i in range(numVertices):
             self.adjacencyMatrix[i] = set()
+            
+            # Create node
             self.idToNode[i] = Node(i)
         
         for src, dst in edges:
             self.adjacencyMatrix[src].add(dst)
+
+            # Add incoming neighbors to node unchecked set
+            self.idToNode[dst].unchecked.add(src)
+
+        self.computeLevels()
+        self.selectParent()
+
+    def computeLevels(self):
+        """Computes and sets level for each node."""
+        queue = deque()
+        queue.append((self.srcNode, 0))
+        visited = {self.srcNode}
+        
+        while queue:
+            currId, currLevel = queue.popleft()
+            self.idToNode[currId].level = currLevel
+
+            for neighbor in self.adjacencyMatrix[currId]:
+                if neighbor not in visited:
+                    queue.append((neighbor, currLevel + 1))
+                    visited.add(neighbor)
+    
+    def selectParent(self):
+        """Select a parent for each node from unchecked set."""
+        for _, node in self.idToNode.items():
+            if node.unchecked:
+                # Move an incoming node as parent
+                node.parent = node.unchecked.pop()
+            
