9d790b588d8ff26773a957cc11c0a2d77afd8494
achitta <46438987+achitta@users.noreply.github.com>
2022-03-28 21:21:25 -0400

    deleteEdge, updateLevel, findNewParent functionality

--- es_tree.py
+++ es_tree.py
@@ -1,11 +1,12 @@
 from collections import deque
+import math
 
 class Node:
     def __init__(self, id):
         # Integers
         self.id = id
-        self.level = None
-        self.parent = None
+        self.level = -1
+        self.parent = -1
 
         # Sets 
         self.nonNeighbors = set()
@@ -24,6 +25,7 @@ class EsTree:
         self.adjacencyMatrix = {} # Map from node id to set of neighbor ids
         self.idToNode = {} # Map from node id to node object
         self.srcNode = srcNode # Source node id
+        self.numVertices = numVertices
         
         for i in range(numVertices):
             self.adjacencyMatrix[i] = set()
@@ -41,7 +43,65 @@ class EsTree:
         self.selectParent()
 
     def deleteEdge(self, src, dst):
-        pass
+        # Remove edge in adjacency matrix
+        self.adjacencyMatrix[src].remove(dst)
+
+        dstNode = self.idToNode[dst]
+        if src in dstNode.nonNeighbors:
+            dstNode.nonNeighbors.remove(src)
+        if src in dstNode.unchecked:
+            dstNode.unchecked.remove(src)
+        if dstNode.parent == src:
+            dstNode.parent = -1
+
+        self.updateLevel(dstNode)
+
+    def updateLevel(self, currNode: Node):
+        # Get the parent node
+        parentNode = None
+        if currNode.parent != -1:
+            parentNode = self.idToNode[currNode.parent]
+        
+        # If currNode has lost its parent, need to update
+        if currNode.parent == -1 or parentNode.level >= currNode.level:
+            self.findNextParent(currNode)
+            # If no parent can be found
+            if currNode.parent == -1:
+                # Move everything from nonNeighbors to unchecked
+                for n_id in currNode.nonNeighbors:
+                    currNode.unchecked.add(n_id)
+                currNode.nonNeighbors.clear()
+
+                # Increment level
+                currNode.level += 1
+                if currNode.level >= self.numVertices:
+                    currNode.level = math.inf
+                
+                # Recursively call update level on neighbors
+                if currNode.level != math.inf:
+                    self.updateLevel(currNode)
+                
+                for n_id in self.adjacencyMatrix[currNode.id]:
+                    neighbor = self.idToNode[n_id]
+                    self.updateLevel(neighbor)       
+
+    def findNextParent(self, currNode: Node):
+        """Sets id of parent or -1 if no parent can be found."""
+        for n_id in currNode.unchecked:
+            neighbor = self.idToNode[n_id]
+            # If this neighbor has a level that is >= current level
+            # then we can confirm that this neighbor cannot serve as the
+            # parent currently
+            #
+            # Else, make the node a parent of the current node
+            if neighbor.level >= currNode.level:
+                currNode.unchecked.remove(n_id)
+                currNode.nonNeighbors.add(n_id)
+            else:
+                currNode.unchecked.remove(n_id)
+                currNode.parent = n_id
+                return
+        currNode.parent = -1
 
     def computeLevels(self):
         """Computes and sets level for each node."""
